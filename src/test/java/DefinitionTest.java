1de0052d160ed7b6f9982ec410e6fb4bc161f6f1
= <blakescala@gmail.com>
2016-09-16 10:09:01 -0700

    write test - getdefinition function returns definition in definitesttest

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -5,9 +5,15 @@ import java.time.LocalDateTime;
 public class DefinitionTest {
 
 
-@Test
+  @Test
   public void Definition_instantiatesCorrectly_true() {
     Definition testDefinition = new Definition("a critical examination, observation, or evaluation");
     assertEquals(true, testDefinition instanceof Definition);
+  }
+
+  @Test
+  public void Definition_returnsDefinition_String() {
+    Definition testDefinition = new Definition("a critical examination, observation, or evaluation");
+    assertEquals("a critical examination, observation, or evaluation", testDefinition.getDefinition());
   }
 }
