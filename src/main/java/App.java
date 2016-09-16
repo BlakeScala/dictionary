a9db43247d8f19c419033ce23de5b7683cac7510
= <blakescala@gmail.com>
2016-09-16 10:43:53 -0700

    add post method that adds words to index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,20 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      String wordInput = request.queryParams("wordInput");
+      Word word = new Word(wordInput);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/:getWord", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-word.vtl");
+      return new ModelAndView(model, layout);
+    })
   }
 }
